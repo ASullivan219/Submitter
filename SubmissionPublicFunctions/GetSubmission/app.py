70ce28946883188eb1ecc0e2de986ec512581826
Alex <a.sullivan219@protonmail.com>
2022-07-10 21:32:30 -0700

    Test added

--- SubmissionPublicFunctions/GetSubmission/app.py
+++ SubmissionPublicFunctions/GetSubmission/app.py
@@ -1,10 +1,13 @@
+from curses.ascii import SUB
 import json
 import os
 import boto3
 S3_CLIENT = boto3.client('s3')
 DYNAMO_CLIENT = boto3.client('dynamodb')
 SUBMISSION_TABLE = os.environ.get('SUBMISSION_TABLE')
-BUCKET_NAME = os.environ.get('BUCEKT_NAME')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+DEBUG = os.environ.get("DEBUG")
+
 
 
 def get_post_url(key: str) -> dict:
@@ -16,14 +19,15 @@ def get_post_url(key: str) -> dict:
         submissionId
     Return: presigned URL and metadata
     """
-
+    BUCKET_NAME = os.environ.get('BUCKET_NAME')
     try:
         response = S3_CLIENT.generate_presigned_post(
                 Bucket=BUCKET_NAME,
                 Key=key,
                 ExpiresIn=3600
         )
-        print(f"Received Prseigned POST url: {response}")
+        if DEBUG:
+            print(f"Received Prseigned POST url: {response}")
         return response
     except Exception as e:
         print(e)
@@ -39,11 +43,17 @@ def get_submission(submissionId: str) -> dict:
     """
 
     try:
+        SUBMISSION_TABLE = os.environ.get('SUBMISSION_TABLE')
         response = DYNAMO_CLIENT.get_item(
+            TableName = SUBMISSION_TABLE,
             Key={
-                'submissionId': submissionId
+                'submissionId': {
+                    'S': submissionId
+                }
             }
         )
+        if DEBUG:
+            print(response)
         return response.get('Item')
     except Exception as e:
         print(e)
@@ -56,6 +66,14 @@ def lambda_handler(event, context):
 
     queryStringParams = event.get("queryStringParameters")
     submissionId = queryStringParams.get('submissionId')
+    
+    
+    if not submissionId:
+        return {
+            'statusCode': 404,
+            'body':"submissionID not found"
+        }
+
     submission = get_submission(submissionId=submissionId)
 
     if submission:
@@ -69,7 +87,9 @@ def lambda_handler(event, context):
             'submissionInfo': 'submissionId invalid'
         }
        
-
+    if DEBUG:
+        print(body)
+    
     return {
         "statusCode": statusCode,
         "body": json.dumps(body)
